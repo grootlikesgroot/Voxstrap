1627d15bf51b705d564d1d817188f6cb1543670c
grootlikesgroot <grootaltmail@gmail.com>
2026-05-14 17:20:12 +0530

    Delete delete_langs.ps1

--- delete_langs.ps1
+++
@@ -1,3 +0,0 @@
-$dir = "c:\Users\Atul Bhandarkar\OneDrive\Desktop\Voxstrap\GrootInjectionLogic\Voxstrap\Voxstrap\Resources"
-Get-ChildItem -Path $dir -Filter "Strings.*.resx" -Recurse | Where-Object { $_.Name -ne "Strings.resx" } | Remove-Item -Force
-Write-Host "Deleted localized resx files successfully!"
